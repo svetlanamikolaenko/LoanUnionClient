fe2bf9c6e6bf03db669c4f7cf87abfe97b1f130f
Svitlana <svetlana.mikolaenko@gmail.com>
2019-02-10 18:19:44 +0200

    added component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CustomersComponent } from './customers/customers.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { CustomerComponent } from './customer/customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomersComponent,
+    FooterComponent,
+    HeaderComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule
