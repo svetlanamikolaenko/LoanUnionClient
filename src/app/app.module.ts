87fdffb9a1880ab7b9f64e5238467e16ab3ebf54
Svitlana <svetlana.mikolaenko@gmail.com>
2019-02-22 22:35:44 +0200

    authorization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,24 @@ import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 
 //services
-import {CustomerService} from './customer.service';
+import { CustomerService } from './customer.service';
 import { AppRouterModule } from './app-router.module';
 import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from './auth.service';
 
 //material design
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCheckboxModule, MatInputModule, MatCardModule, MatSelectModule, MatTableModule, MatToolbarModule, MatDialogModule, MatListModule} from '@angular/material';
 
+//components
 import { NewCustomerComponent } from './new-customer/new-customer.component';
 import { UpdateCustomerComponent } from './update-customer/update-customer.component';
+import { DeleteCustomerComponent } from './delete-customer/delete-customer.component';
+import { RegisterComponent} from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 //forms
-import {ReactiveFormsModule} from '@angular/forms';
-import { DeleteCustomerComponent } from './delete-customer/delete-customer.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -31,21 +35,24 @@ import { DeleteCustomerComponent } from './delete-customer/delete-customer.compo
     HeaderComponent,
     NewCustomerComponent,
     UpdateCustomerComponent,
-    DeleteCustomerComponent
+    DeleteCustomerComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    ReactiveFormsModule,
+    
     //material design
     BrowserAnimationsModule, MatButtonModule, MatTableModule,
     MatInputModule, MatCardModule, MatSelectModule,
     MatToolbarModule,MatListModule,
     MatDialogModule,
+    ReactiveFormsModule,FormsModule,
     AppRouterModule
   ],
   entryComponents:[UpdateCustomerComponent],
-  providers: [CustomerService],
+  providers: [CustomerService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
